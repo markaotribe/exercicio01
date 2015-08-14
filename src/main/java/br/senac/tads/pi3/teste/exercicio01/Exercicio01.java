71cc0361fb1802564276e7c2e6700fe5f8309e40
marcos.vcnmarcos <marcos.vcnmarcos@CAS419281C14718.senacsp.edu.br>
2015-08-14 20:06:19 -0300

    Primeiro commit (teste)

---
+++ src/main/java/br/senac/tads/pi3/teste/exercicio01/Exercicio01.java
@@ -0,0 +1,13 @@
+
+package br.senac.tads.pi3.teste.exercicio01;
+
+/**
+ *
+ * @author marcos.vcnmarcos
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        System.out.println("RÉLOU UORD");   
+    }
+    
+}
