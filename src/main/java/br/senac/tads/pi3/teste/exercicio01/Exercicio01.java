555f54dcb9b31a7e49eb7a2a53e16ae155d543fc
marcos.vcnmarcos <marcos.vcnmarcos@CAS419281C14718.senacsp.edu.br>
2015-08-14 21:52:49 -0300

    Atualização exercicio

--- src/main/java/br/senac/tads/pi3/teste/exercicio01/Exercicio01.java
+++ src/main/java/br/senac/tads/pi3/teste/exercicio01/Exercicio01.java
@@ -7,7 +7,7 @@ package br.senac.tads.pi3.teste.exercicio01;
  */
 public class Exercicio01 {
     public static void main(String[] args) {
-        System.out.println("RÉLOU UORD");   
+        System.out.println("hello Word!!!");   
     }
     
 }
